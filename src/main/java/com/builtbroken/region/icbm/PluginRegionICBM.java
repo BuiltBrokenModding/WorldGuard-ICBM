3f2524b361ba4d942fdf134f5017300210962c7e
Robert S <rseifert.phone@gmail.com>
2014-06-27 01:31:09 -0400

    Setup event handling

--- src/main/java/com/builtbroken/region/icbm/PluginRegionICBM.java
+++ src/main/java/com/builtbroken/region/icbm/PluginRegionICBM.java
@@ -1,12 +1,28 @@
 package com.builtbroken.region.icbm;
 
+import icbm.api.explosion.ExplosionEvent.PreExplosionEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.event.ForgeSubscribe;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.mewin.WGCustomFlags.WGCustomFlagsPlugin;
+import com.sk89q.worldedit.Vector;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 
+/**
+ * Bukkit plugin to help support ICBM interaction with WorldGuard
+ * 
+ * @since 6/26/2014
+ * @author Robert Seifert
+ */
 public class PluginRegionICBM extends JavaPlugin
 {
 	private static PluginRegionICBM instance;
@@ -22,14 +38,15 @@ public class PluginRegionICBM extends JavaPlugin
 	{
 		instance = this;
 		logger().info("Enabled!");
+		MinecraftForge.EVENT_BUS.register(this);
 	}
-	
+
 	@Override
 	public void onDisable()
 	{
 		logger().info("Disabled!");
 	}
-	
+
 	/** Logger used by the plugin, mainly just prefixes everything with the name */
 	public PluginLogger logger()
 	{
@@ -40,7 +57,7 @@ public class PluginRegionICBM extends JavaPlugin
 		}
 		return logger;
 	}
-	
+
 	/** Gets the worldguard plugin currently loaded */
 	protected WorldGuardPlugin getWorldGuard()
 	{
@@ -65,5 +82,19 @@ public class PluginRegionICBM extends JavaPlugin
 		}
 
 		return (WGCustomFlagsPlugin) plugin;
+	}
+	
+	@ForgeSubscribe
+	public void preExplosion(PreExplosionEvent event)
+	{
+		System.out.println("Boom Time");
+		WorldGuardPlugin guard = getWorldGuard();
+		if (guard != null)
+		{
+			Vector vec = new Vector(event.x, event.y, event.z);
+			World world = Bukkit.getWorld("DIM" + event.world.provider.dimensionId);
+			RegionManager manager = guard.getRegionManager(world);
+			ApplicableRegionSet set = manager.getApplicableRegions(vec);			
+		}
 	}
 }
